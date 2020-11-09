cdd2b187e4a5754205f65526737d64a8c9bee72a
andrew2020wit <andrew2020wit@gmail.com>
2020-11-09 13:52:30 +0200

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: { main: "./src/index.js", analytics: "./src/analytics.js" },
@@ -6,4 +9,8 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HTMLWebpackPlugin({ template: "./index.html" }),
+    new CleanWebpackPlugin(),
+  ],
 };
