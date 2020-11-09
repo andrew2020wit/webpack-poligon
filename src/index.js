9d3f29015cdbdc113b4d35d617064623366c0518
andrew2020wit <andrew2020wit@gmail.com>
2020-11-09 14:40:01 +0200

    file-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import js1 from "./assets/1.json";
+import logo from "./assets/webpack.png";
 import { getHello } from "./lib";
 import "./styles/styles.css";
 
 getHello("Bob");
+
+console.log("logo-name: ", logo);
+console.log("js1: ", js1);
--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,23 @@ module.exports = {
   context: path.resolve(__dirname, "src"),
   entry: { main: "./index.js", analytics: "./analytics.js" },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    // default extensions
+    extensions: [".js", ".json"],
+    alias: { "@styles": path.resolve(__dirname, "src/styles/") },
   },
   plugins: [
     new HTMLWebpackPlugin({ template: "./index.html" }),
     new CleanWebpackPlugin(),
   ],
-  module: { rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }] },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|jpeg|svg|gif)$/, use: ["file-loader"] },
+      { test: /\.(ttf|woff|woff2|eot)$/, use: ["file-loader"] },
+    ],
+  },
 };
